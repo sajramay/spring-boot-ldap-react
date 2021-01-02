b41c5b849748ec750e77535358eccd218db740db
thewaterwalker <sramay+github@gmail.com>
2021-01-02 20:20:13 +0000

    Initial cut of AWS CDK deployment plan

---
+++ awscdk/bin/awscdk.ts
@@ -0,0 +1,10 @@
+#!/usr/bin/env node
+import 'source-map-support/register';
+import * as cdk from '@aws-cdk/core';
+import { AwscdkStack } from '../lib/awscdk-stack';
+
+const myenv = {account: process.env.CDK_DEFAULT_ACCOUNT, 
+    region: process.env.CDK_DEFAULT_REGION}
+
+const app = new cdk.App();
+new AwscdkStack(app, 'AwscdkStack', {env: myenv});
